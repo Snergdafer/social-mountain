ae169af6ef9b732b79ddcb120648c3cad998d910
Jesse Etherington <jesse.etherington@gmail.com>
2021-01-26 16:09:06 -0700

    did the assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import axios from "axios"
 import './App.css';
-
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from "./Post/Post.js"
 
 class App extends Component {
   constructor() {
@@ -19,31 +19,69 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios.get("https://practiceapi.devmountain.com/api/posts")
+    .then( posts => {
+      // console.log(posts)
+      this.setState({
+        posts: posts.data
+      })
+    }).catch( err => {
+      console.log("Error getting all posts")
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+  axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+  .then( res => {
+    this.setState({
+      posts: res.data
+    })
+  })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then( res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts/`, {text})
+    .then( res => {
+      this.setState({
+        posts: res.data
+      })
+    })
   }
 
   render() {
     const { posts } = this.state;
-
+    console.log(this.state.posts)
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
 
-          <Compose />
+          <Compose 
+            createPostFn={this.createPost}
+          />
+
+          {this.state.posts.map( elem => (
+
+              <Post 
+                key={elem.id}
+                id={elem.id} 
+                text={elem.text} 
+                date={elem.date} 
+                updatePostFn={this.updatePost} 
+                deletePostFn={this.deletePost}
+              />
+
+          ))}
           
         </section>
       </div>
